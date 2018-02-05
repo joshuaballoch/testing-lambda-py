b50dc44e484d4564e002241222efb9a980b29e90
Joshua Balloch <joshuaballoch@gmail.com>
2018-02-05 20:13:52 +0000

    Create sample Lambda function with tests:
    * Tries out using moto to mock out s3
    * Function just adds an item to a dynamodb table and moves file
      from incoming to processed

--- handler.py
+++ handler.py
@@ -1,13 +1,35 @@
+import boto3
 import json
+import re
+
+def read_file(s3_client, bucket, key):
+    return s3_client.get_object(
+        Bucket=bucket,
+        Key=key
+    )['Body'].read().decode('utf-8')
+
+def move_object_to_processed(s3_client, original_bucket, original_key):
+    new_key = re.sub("incoming\/", "processed/", original_key)
+    s3_client.copy_object(
+        Bucket=original_bucket,
+        Key=new_key,
+        CopySource={'Bucket': original_bucket, 'Key': original_key}
+    )
+    s3_client.delete_object(Bucket=original_bucket, Key=original_key)
 
 def call(event, context):
-    body = {
-        "message": "Hello World!"
-    }
+    s3_client = boto3.client('s3')
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    table = boto3.resource('dynamodb', region_name='us-east-1').Table("my-transactions-table")
+    txn_id = re.search("incoming\/transaction-(\d*).txt", key).group(1)
+    table.put_item(
+        Item={
+            'transaction_id': txn_id,
+            'body': read_file(s3_client, bucket, key)
+        }
+    )
 
-    return response
+    move_object_to_processed(s3_client, bucket, key)
