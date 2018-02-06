797a59e81b275fec6dcd07308db1a73a1bdce307
Joshua Balloch <joshuaballoch@gmail.com>
2018-02-06 16:05:11 +0000

    Revert code back to basic starting point

--- handler.py
+++ handler.py
@@ -1,26 +1,2 @@
-import boto3
-import json
-import re
-
-def read_file(s3_client, bucket, key):
-    return s3_client.get_object(
-        Bucket=bucket,
-        Key=key
-    )['Body'].read().decode('utf-8')
-
-def move_object_to_processed(s3_client, original_bucket, original_key):
-    new_key = re.sub("incoming\/", "processed/", original_key)
-    s3_client.copy_object(
-        Bucket=original_bucket,
-        Key=new_key,
-        CopySource={'Bucket': original_bucket, 'Key': original_key}
-    )
-    s3_client.delete_object(Bucket=original_bucket, Key=original_key)
-
 def call(event, context):
-    s3_client = boto3.client('s3')
-    record = event['Records'][0]
-    bucket = record['s3']['bucket']['name']
-    key = record['s3']['object']['key']
-
-    move_object_to_processed(s3_client, bucket, key)
+    print("hello world!")
