2f67deab63cb47403e52429ab504a9c341bcaed0
Joshua Balloch <joshuaballoch@gmail.com>
2018-02-07 10:23:19 +0000

    Implement S3 functionality (moves incoming files to processed)

--- handler.py
+++ handler.py
@@ -1,2 +1,19 @@
+import boto3
+import re
+
+def move_object_to_processed(s3_client, original_bucket, original_key):
+    new_key = re.sub("incoming\/", "processed/", original_key)
+    s3_client.copy_object(
+        Bucket=original_bucket,
+        Key=new_key,
+        CopySource={'Bucket': original_bucket, 'Key': original_key}
+    )
+    s3_client.delete_object(Bucket=original_bucket, Key=original_key)
+
 def call(event, context):
-    print("hello world!")
+    s3_client = boto3.client('s3')
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
+
+    move_object_to_processed(s3_client, bucket, key)
--- test_handler.py
+++ test_handler.py
@@ -1,8 +1,46 @@
 import boto3
 import pytest
+from botocore.exceptions import ClientError
+from moto import mock_s3
 from handler import call
 
 ## Test Setup Functions
 
 def test_handler_moves_incoming_object_to_processed():
-    assert call(None, None) == None
+    with mock_s3():
+        # Create the bucket
+        conn = boto3.resource('s3', region_name='us-east-1')
+        conn.create_bucket(Bucket="some-bucket")
+        # Add a file
+        boto3.client('s3', region_name='us-east-1').put_object(Bucket="some-bucket", Key="incoming/transaction-0001.txt", Body="Hello World!")
+
+        # Run call with an event describing the file:
+        call(s3_object_created_event("some-bucket", "incoming/transaction-0001.txt"), None)
+
+        # Assert the original file doesn't exist
+        with pytest.raises(ClientError) as e_info:
+            conn.Object("some-bucket", "incoming/transaction-0001.txt").get()
+            assert e_info.response['Error']['Code'] == 'NoSuchKey'
+
+        # Check that it exists in `processed/`
+        obj = conn.Object("some-bucket", "processed/transaction-0001.txt").get()
+        assert obj['Body'].read() == b'Hello World!'
+
+def s3_object_created_event(bucket_name, key):
+    # NOTE: truncated event object shown here
+    return {
+      "Records": [
+        {
+          "s3": {
+            "object": {
+              "key": key,
+            },
+            "bucket": {
+              "name": bucket_name,
+            },
+          },
+        }
+      ]
+    }
+
+
